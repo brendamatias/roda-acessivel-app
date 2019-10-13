38681fc243a2be3b9c7a645bf39c01ca62c04ae0
Brenda Matias <brendamatias.sobral@gmail.com>
2019-10-13 01:54:26 -0300

    feat: added routes

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinerGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinerGradient).attrs({
+  colors: ['#557cf2', '#3B59B2'],
+})`
+  flex: 1;
+`;
